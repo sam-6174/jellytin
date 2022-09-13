0b9878956c2ac4e6f291daa8428d02e4fdb77f73
Sam Brownlow <sam-brownlow>
2022-09-12 20:45:20 -0700

    all.sh: support ps

--- all.sh
+++ all.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 
 UP_OR_DOWN="$1"
-if [ "$UP_OR_DOWN" != "up" ] && [ "$UP_OR_DOWN" != "down" ]; then
-  echo "all.sh expected \$1 of 'up' or 'down' but found '$UP_OR_DOWN'" >&2
+if [ "$UP_OR_DOWN" != "up" ] && [ "$UP_OR_DOWN" != "down" ] && [ "$UP_OR_DOWN" != "ps" ]; then
+  echo "all.sh expected \$1 of 'up' or 'down' or 'ps' but found '$UP_OR_DOWN'" >&2
   exit 1
 fi
 
@@ -20,7 +20,7 @@ if [ "$UP_OR_DOWN" = "up" ]; then
 fi
 
 for COMPOSE_DIR in 'cloudflare_tunnel' 'authentik' 'nginx_proxy_manager'; do
-  echo "Bringing $UP_OR_DOWN '$COMPOSE_DIR'..."
+  echo "$UP_OR_DOWN '$COMPOSE_DIR'..."
   (cd "$DIR_PATH/$COMPOSE_DIR" && docker-compose "$UP_OR_DOWN" ${COMPOSE_ARGS[@]+"${COMPOSE_ARGS[@]}"}) || echo '^^ ERROR ^^'
   echo ''
 done
