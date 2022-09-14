c5ea678bde47a728430b92ca8a595ca1590c9818
Sam Brownlow <sam-brownlow>
2022-09-14 13:58:05 -0700

    all.sh: tweak script

--- all.sh
+++ all.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-UP_OR_DOWN="$1"
-if [ "$UP_OR_DOWN" != "up" ] && [ "$UP_OR_DOWN" != "down" ] && [ "$UP_OR_DOWN" != "ps" ]; then
-  echo "all.sh expected \$1 of 'up' or 'down' or 'ps' but found '$UP_OR_DOWN'" >&2
-  exit 1
-fi
-
 dir_path() {
   # https://stackoverflow.com/a/43919044
   a="/$0"; a="${a%/*}"; a="${a:-.}"; a="${a##/}/"; BINDIR=$(cd "$a"; pwd)
@@ -14,13 +8,7 @@ dir_path() {
 }
 DIR_PATH=`dir_path`
 
-COMPOSE_ARGS=()
-if [ "$UP_OR_DOWN" = "up" ]; then
-  COMPOSE_ARGS+=('-d')
-fi
-
-for COMPOSE_DIR in 'cloudflare_tunnel' 'authentik' 'nginx_proxy_manager'; do
-  echo "$UP_OR_DOWN '$COMPOSE_DIR'..."
-  (cd "$DIR_PATH/$COMPOSE_DIR" && docker-compose "$UP_OR_DOWN" ${COMPOSE_ARGS[@]+"${COMPOSE_ARGS[@]}"}) || echo '^^ ERROR ^^'
+for COMPOSE_DIR in 'cloudflare_tunnel' 'authentik' 'authentik_ldap' 'nginx_proxy_manager'; do
+  (cd "$DIR_PATH/$COMPOSE_DIR" && docker-compose "$@") || echo '^^ ERROR ^^'
   echo ''
 done
