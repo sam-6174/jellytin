e30a5ef9bbd21aa6de7a916a971b043f65c7bee5
Sam Brownlow <sam-brownlow>
2022-09-14 21:21:39 -0700

    all.sh: add comments

--- all.sh
+++ all.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+##
+# This is a helper script to run docker-compose
+# commands against all the stacks in this repo
+#
+# for example:
+#   `./all.sh up -d`
+#   `./all.sh ps`
+#   `./all.sh down`
+##
+
 dir_path() {
   # https://stackoverflow.com/a/43919044
   a="/$0"; a="${a%/*}"; a="${a:-.}"; a="${a##/}/"; BINDIR=$(cd "$a"; pwd)
